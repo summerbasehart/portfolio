526184fbcf93f505fad1b6e39977b9ca2b4cf1a4
unknown <summerbasehart@gmail.com>
2019-06-28 22:33:21 -0700

    added firebase for hosting

---
+++ app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA--0EjBwdIqozsW8LYYfitBf4IkxBEFYc",
+    authDomain: "portfolio-summerbasehart.firebaseapp.com",
+    databaseURL: "https://portfolio-summerbasehart.firebaseio.com",
+    projectId: "portfolio-summerbasehart",
+    storageBucket: "",
+    messagingSenderId: "891678579533",
+    appId: "1:891678579533:web:4908ba52b5262963"
+};
+
+firebase.initializeApp(firebaseConfig);
