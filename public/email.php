8aeffd672c774564aed97a9b0996b3730880c431
Summer GB <47164799+summerbasehart@users.noreply.github.com>
2024-01-12 10:29:08 -0800

    update bump

--- public/email.php
+++ public/email.php
@@ -14,7 +14,7 @@
     mail($to, $subject, $emailbody, $headers);
     echo "<h2 style='background-color: black; color:white; text-align:center'>Thank you {$first_name}, for contacting Summer Basehart.</h2><br>
     <h3>We will contact you at <b><u>{$email}</b></u> as soon as possible</h3>";
-    $url='https://summerbasehart.com/contact';
+    $url='https://summerbasehart.com/';
         sleep(5);
     echo '<script>setTimeout(function(){window.location.href = 
     "'.$url.'"}, 2 * 2000);</script>';
